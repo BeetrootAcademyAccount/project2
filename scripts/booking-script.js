5df2bc816695dcc8907971aa26b210067a48996f
elnurnabivi <elnurnabivi@gmail.com>
2023-09-15 23:11:02 +0200

    Added Booking page and its components

---
+++ scripts/booking-script.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  $(".booking-slider").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
