707969b92455ac31ae9e99ce86abc4664d43d5c3
elnurnabivi <elnurnabivi@gmail.com>
2023-09-21 17:07:42 +0200

    Added js functionality

--- scripts/booking-script.js
+++ scripts/booking-script.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     pauseOnHover: true,
     responsive: [
       {
@@ -15,13 +15,25 @@ $(document).ready(() => {
           slidesToScroll: 1,
         },
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
     ],
   });
+});
+
+const btnHeart = document.querySelector(".btn-heart");
+btnHeart.style.transition = "transform 0.2s ease";
+
+btnHeart.addEventListener("mouseover", function () {
+  btnHeart.style.color = "red";
+});
+
+btnHeart.addEventListener("mouseout", function () {
+  btnHeart.style.color = "";
+});
+
+let isClicked = false;
+btnHeart.addEventListener("click", function () {
+  if (!isClicked) {
+    btnHeart.style.color = "red";
+    isClicked = true;
+  }
 });
